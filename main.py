fafbe3a54297efda2d46f50957193c50b3e116f9
cypherkrish <av.krishnamurthy007@gmail.com>
2025-02-23 23:36:12 +0530

    fisrtcommig

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from utils.common import PayLoad
+from utils.operations import add_numbers
+
+app = FastAPI()
+
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World", "Status":"The app is running file"}
+
+
+@app.post("/add")
+def add_numbers_api(payload: PayLoad):
+    result = add_numbers(payload.number_one, payload.number_two)
+    return {"Hello": "World", "Sum":f"{result}"}
+    
+
---
+++ utils/common.py
@@ -0,0 +1,5 @@
+from pydantic import BaseModel
+
+class PayLoad (BaseModel):
+    number_one: int
+    number_two: int
